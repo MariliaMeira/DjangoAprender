26420e9f31ba36cea118ec7f4a6e3590e03328a2
LUCASRENAA_IFPE <lrms2@discente.ifpe.edu.br>
2022-08-21 19:35:41 -0300

    ensinando a passar dados via dicionario com django para os templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "myapp/home.html")
+    nome_value = request.user
+    id_value = request.user.id
+
+    #dicionario exemplo
+    #dados = {"chave":"valor","key":"value"}
+
+
+    dados = {"nome":nome_value,"id":id_value}
+    print(type(dados))
+    return render(request, "myapp/home.html",dados)
 def funcao_views(request):
     return render(request, 'myapp/passo2.html')
 def funcao2(request):
     return render(request, 'passo3.html')
 def funcao3(request):
     return render(request, 'passo4.html')
+
+"""
+def soma(a,b):
+    return a+b
+"""
